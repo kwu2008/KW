49951e594e806de109682ea244e77088cf19d1fb
Kevin Wu <YWu2@slb.com>
2021-02-09 10:23:12 -0600

    Change 0001

--- BitStuckPrevention/bsp.py
+++ BitStuckPrevention/bsp.py
@@ -1,1 +1,27 @@
-print('Hello bsp')
+import sys
+
+def BSP_LoadData():
+    print('BSP_LoadData')
+
+
+def BSP_ProcessData():
+    print('BSP_ProcessData')
+
+
+def BSP_DisplayData():
+    print('BSP_DisplayData')
+
+
+def BSP_Main():
+    print('BSP_Main')
+    BSP_LoadData()
+    BSP_ProcessData()
+    BSP_DisplayData()
+
+
+
+
+if __name__ == '__main__':
+        BSP_Main()
+
+
